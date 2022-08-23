a4cd9312fc240827d883010f8935a187fd5d1c0a
unknown <sadafzillani98@gmail.com>
2022-08-23 12:28:44 +0530

    third commit

--- player.js
+++ player.js
@@ -2,6 +2,16 @@ const playerArray = document.getElementsByClassName("player-btn");
 for (var i = 0; i < playerArray.length; i++) {
   document.getElementById(playerArray[i].id).onclick = selectClicked;
 }
+function addNametolist(name) {
+  const node = document.createElement("li");
+  const player = document.createTextNode(name);
+  node.appendChild(name);
+}
+
 function selectClicked(event) {
-  console.log(this.id);
+  let playerID = this.id;
+  let parentID = document.getElementById(playerID).parentElement.id;
+  let playerName =
+    document.getElementById(parentID).firstElementChild.innerHTML;
+  addNametoList(playerName);
 }
